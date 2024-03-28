24e411fd503c58e7d2d0c3a9a3edf3333fab21a1
ganesh.oro <ganesh.s@orotron.com>
2024-03-28 13:26:51 +0530

    update responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,10 +3,10 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
-		</div>
+		<div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <Sidebar className='sm:w-1/3' /> {/* Sidebar will take 1/3 of the width on small screens */}
+            <MessageContainer className='sm:w-2/3' /> {/* MessageContainer will take 2/3 of the width on small screens */}
+        </div>
 	);
 };
 export default Home;
